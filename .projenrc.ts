f9406b516123207f27ebd5b864f85f6c518aa127
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2024-10-16 12:30:10 +0100

    chore: disable mergify

--- .projenrc.ts
+++ .projenrc.ts
@@ -85,6 +85,9 @@ const project = new typescript.TypeScriptProject({
       run: 'npx projen shrinkwrap',
     },
   ],
+  githubOptions: {
+    mergify: false,
+  },
 });
 
 project.addPackageIgnore('*.ts');
