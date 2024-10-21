342abeac0175eb63c2e89bdd2548a083633d19c2
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2024-10-21 15:48:35 +0100

    chore: switch to merge queues (#143)
    The branch protection rules have been updated to require merge queue.
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- .projenrc.ts
+++ .projenrc.ts
@@ -85,6 +85,10 @@ const project = new typescript.TypeScriptProject({
       run: 'npx projen shrinkwrap',
     },
   ],
+  githubOptions: {
+    mergify: false,
+    mergeQueue: true,
+  },
 });
 
 project.addPackageIgnore('*.ts');
